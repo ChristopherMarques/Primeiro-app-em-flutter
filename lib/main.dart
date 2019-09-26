fdd2529be1b8a8f22e7ff0fc11878fd55a2e2eef
ChristopherMarques <chriscorrea.marques@gmail.com>
2019-09-26 16:33:11 -0300

    primeira importação

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+import 'models/item.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskController = new TextEditingController();
+
+  void add() {
+    if (newTaskController.text.isEmpty) return;
+
+    setState(() {
+      widget.items.add(
+        Item(title: newTaskController.text, done: false),
+      );
+      newTaskController.text = "";
+      newTaskController.clear();
+    });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskController,
+          keyboardType: TextInputType.text,
+          style: TextStyle(color: Colors.white60, fontSize: 20),
+          decoration: InputDecoration(
+            labelText: "Insira uma nova tarefa...",
+            labelStyle: TextStyle(color: Colors.white38),
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctx, int index) {
+          final item = widget.items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
+            key: Key(item.title),
+            background: Container(
+              color: Colors.redAccent.withOpacity(0.8),
+            ),
+            onDismissed: (direction) {
+              return remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black87,
+      ),
+    );
+  }
+}
